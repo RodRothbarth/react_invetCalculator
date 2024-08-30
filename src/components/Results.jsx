280e90e316714c069cc6b0ff854a2bd0d44b5eac
rodrothbarth <rodrothbarth@gmail.com>
2024-08-29 22:13:39 -0300

    table added to App.tsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,11 +1,44 @@
-import { calculateInvestmentResults } from "../util/investment.js";
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
 export function Results({ formInfo }) {
   const results = calculateInvestmentResults(formInfo);
-  console.log(results);
+  const initianInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
+
   return (
     <>
-      <div>{}</div>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Interest Value</th>
+            <th>Interest (year)</th>
+            <th>Interest Total</th>
+            <th>Interest Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((yearData) => {
+            const totalInterest =
+              yearData.valueEndOfYear -
+              yearData.annualInvestment * yearData.year -
+              initianInvestment;
+            const totaInvested = yearData.valueEndOfYear - totalInterest;
+
+            return (
+              <tr key={yearData.year}>
+                <td>{yearData.year}</td>
+                <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                <td>{formatter.format(yearData.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totaInvested)}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
